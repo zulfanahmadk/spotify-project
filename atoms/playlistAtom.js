2eafe41439f6255efb4b97f8b02f098f81ae0926
zulfanahmadk <zulfanahmadk@gmail.com>
2022-01-31 10:21:32 +0700

    Final Project

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+});
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: '3p2fKbBjk2uJk7v4B04LT6',
+});
